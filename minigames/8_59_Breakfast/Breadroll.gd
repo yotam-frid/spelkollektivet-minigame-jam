5884325879d1c073daaba1cda33fe039fd059927
Hjalte Tagmose <HjalteTagmose@users.noreply.github.com>
2025-08-13 12:51:13 +0200

    some kinda physics movement with fucked visuals

--- minigames/8_59_Breakfast/Breadroll.gd
+++ minigames/8_59_Breakfast/Breadroll.gd
@@ -9,11 +9,12 @@ func _ready() -> void:
 	body_entered.connect(_on_body_entered)
 	
 func _on_body_entered(body: Node) -> void:
-	if body is RigidBody2D:
-		print("Collided with:", body.name)
-		if freeze:
-			var direction = (body.global_position - global_position).normalized()
-			body.linear_velocity = direction * 500 + Vector2.UP * 100
-			body.angular_velocity = 0
-		else: 
-			body.linear_velocity = linear_velocity
+	pass
+	#if body is RigidBody2D:
+		#print("Collided with:", body.name)
+		#if freeze:
+			#var direction = (body.global_position - global_position).normalized()
+			#body.linear_velocity = direction * 500 + Vector2.UP * 100
+			#body.angular_velocity = 0
+		#else: 
+			#body.linear_velocity = linear_velocity
