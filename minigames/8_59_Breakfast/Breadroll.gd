025c55946c5d19b3f5df729f0cd89ca243dc3fdf
Hjalte Tagmose <HjalteTagmose@users.noreply.github.com>
2025-08-13 13:23:45 +0200

    freezer front

--- minigames/8_59_Breakfast/Breadroll.gd
+++ minigames/8_59_Breakfast/Breadroll.gd
@@ -3,7 +3,7 @@ class_name Breadroll
 
 func _ready() -> void:
 	print("Scaling applied to", name)
-	var new_scale = Vector2.ONE * .75 + Vector2.ONE * .5 * randf()
+	var new_scale = Vector2.ONE * .8 + Vector2.ONE * .4 * randf()
 	$Sprite2D.scale = new_scale
 	$Shape.scale = new_scale
 	$Shape2.scale = new_scale
