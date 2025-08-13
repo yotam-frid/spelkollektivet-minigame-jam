a4addaa92848f4ccf1ab200e4b8ba53c182db59f
Hjalte Tagmose <HjalteTagmose@users.noreply.github.com>
2025-08-13 13:05:44 +0200

    fix interactions

--- minigames/8_59_Breakfast/Breadroll.gd
+++ minigames/8_59_Breakfast/Breadroll.gd
@@ -2,19 +2,4 @@ extends RigidBody2D
 class_name Breadroll
 
 func _ready() -> void:
-	print("body.breadrooll: ", name)
 	set_global_scale(Vector2.ONE * randf())
-	contact_monitor = true
-	max_contacts_reported = 8
-	body_entered.connect(_on_body_entered)
-	
-func _on_body_entered(body: Node) -> void:
-	pass
-	#if body is RigidBody2D:
-		#print("Collided with:", body.name)
-		#if freeze:
-			#var direction = (body.global_position - global_position).normalized()
-			#body.linear_velocity = direction * 500 + Vector2.UP * 100
-			#body.angular_velocity = 0
-		#else: 
-			#body.linear_velocity = linear_velocity
